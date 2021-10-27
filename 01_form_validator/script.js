8449f4d92b3456caf800d9c2cc19b4c63e6b6370
Daniel Bodmer <danielbodmer@fastmail.com>
2021-10-27 17:07:56 +0100

    update script.js of form validtor

--- 01_form_validator/script.js
+++ 01_form_validator/script.js
@@ -4,10 +4,27 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+function showError(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+
+  formControl.className = 'form-control error';
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = 'form-control success';
+}
+
+// event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   
   if (username.value === '') {
-    alert('Username is required.')
+    showError(username, 'Username is required.');
+  } else {
+    showSuccess(username);
   }
 })
